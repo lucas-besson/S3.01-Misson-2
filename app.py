c871d013d6b800034898f1aac85670f5ed026757
Lucas Besson <lucas.besson02@edu.univ-fcomte.fr>
2023-09-29 15:36:40 +0200

    First Commit

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-from flask import Flask
+from flask import Flask, request, render_template, redirect, flash, session, g
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def show_accueil():
+    return render_template('layout.html')
+
 
 
 if __name__ == '__main__':
