3ab3ce7f7647e9c2101747bd9a8b16bd6fe9de9f
Lucas Besson <lucas.besson02@edu.univ-fcomte.fr>
2023-10-22 20:59:01 +0200

    Ajout filtre par Categorie Pathologie

--- models/dao_filtre.py
+++ models/dao_filtre.py
@@ -3,17 +3,18 @@ from flask import *
 
 from connexion_db import get_db
 
-def filtrer_patients(filter_word, types):
+def filtrer_patients(filter_word, types, filter_types):
     connection = get_db()
     cursor = connection.cursor()
-    sql = '''SELECT nom, nomPathologie
+    sql = '''SELECT nom, nomPathologie, nomCategoriePathologie
              FROM Patient
-             INNER JOIN estMaladeDe emd on Patient.idPatient = emd.idPatient
-             INNER JOIN Pathologie P on emd.idPathologie = P.idPathologie'''
+             INNER JOIN estMaladeDe emd ON Patient.idPatient = emd.idPatient
+             INNER JOIN Pathologie P ON emd.idPathologie = P.idPathologie
+             INNER JOIN categoriePathologie cP ON P.idCategoriePathologie = cP.idCategoriePathologie'''
     list_param = []
     condition_and = ""
 
-    if filter_word or types:
+    if filter_word or types or filter_types:
         sql = sql + " WHERE "
 
     if filter_word:
@@ -27,6 +28,18 @@ def filtrer_patients(filter_word, types):
         last_item = types[-1]
         for item in types:
             sql = sql + " P.idPathologie = %s "
+            if item != last_item:
+                sql = sql + " OR "
+            list_param.append(item)
+        sql = sql + ")"
+
+    if filter_types:
+        if filter_word or types:
+            condition_and = " AND "
+        sql = sql + condition_and + "("
+        last_item = filter_types[-1]
+        for item in filter_types:
+            sql = sql + " cP.idCategoriePathologie = %s "
             if item != last_item:
                 sql = sql + " OR "
             list_param.append(item)
