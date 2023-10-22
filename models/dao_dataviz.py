b80456b0bf06b448d27af01eb00d21b80a352183
Eloise <e.launay512@gmail.com>
2023-10-22 22:08:46 +0200

    Style DataViz

--- models/dao_dataviz.py
+++ models/dao_dataviz.py
@@ -3,7 +3,7 @@ from flask import *
 
 from connexion_db import get_db
 
-def dataviz_find():
+def dataviz_find_categorie():
     connection = get_db()
     try:
         cursor = connection.cursor()
@@ -15,4 +15,18 @@ def dataviz_find():
         cursor.execute(sql)
         return cursor.fetchall()
     except ValueError:
-        abort(400, 'error requete find_medicament')
+        abort(400, 'error requete dataviz_find_categorie')
+
+def dataviz_find_indice():
+    connection = get_db()
+    try:
+        cursor = connection.cursor()
+        sql = ''' SELECT cP.nomCategoriePathologie AS LABELLE,COUNT(idPathologie) AS TOTAL
+                      FROM Pathologie
+                      INNER JOIN categoriePathologie cP on cP.idCategoriePathologie = Pathologie.idCategoriePathologie
+                      GROUP BY cP.idCategoriePathologie
+                      ORDER BY cP.idCategoriePathologie DESC ;'''
+        cursor.execute(sql)
+        return cursor.fetchall()
+    except ValueError:
+        abort(400, 'error requete dataviz_find_indice')
